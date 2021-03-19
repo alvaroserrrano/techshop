f669c61c206368fbf5951f96ac38d957956c0345
Alvaro <alvaro.serrano@wmich.edu>
2021-03-19 17:12:22 +0100

    added constants for redux actions

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 urlpatterns = [
   path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('register/', views.registerUser, name='register'), 
-  path('profile/' views.getUserProfile,name='user-profile'),
-  path('profile/update/', views.updateProfile, name='user-profile-update'),
+  path('profile/' ,views.getUserProfile,name='user-profile'),
+  path('profile/update/', views.updateUserProfile, name='user-profile-update'),
   path('<str:pk>/', views.getUserById, name='user'),
   path('update/<str:pk>/', views.updateUser, name='user-update'),
   path('delete/<str:pk>', views.deleteUser, name='user-delete')
